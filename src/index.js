b3b324483205f2c185352fad56747ea26cefd61b
HsuTingHuan <s780609@gmail.com>
2022-02-22 23:34:35 +0800

    chore: researching react-redux

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { Provider } from 'react-redux';
-import store from "./app/store";
+//import store from "./app/store";
 
 import Button from "./components/Button";
 import { Counter } from "./features/counter/Counter";
+
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
+
+let store = createStore(todoApp)
 
 let element = <Button>Hello World</Button>
 
 ReactDOM.render(
     <Provider store={store}>
         {element}
-        <Counter></Counter>
+        {/* <Counter></Counter> */}
+        <App></App>
     </Provider>,
     document.getElementById("root"));
