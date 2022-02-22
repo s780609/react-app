62c2108a1e7db3a5dbb7f0cf8eeedef6565fed36
HsuTingHuan <s780609@gmail.com>
2022-02-22 20:47:03 +0800

    feat: install react-redux,and add a counterSlice

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Provider } from 'react-redux';
+import store from "./app/store";
+
 import Button from "./components/Button";
+import { Counter } from "./features/counter/Counter";
 
-let element = <Button>Hello World GGC</Button>
+let element = <Button>Hello World</Button>
 
-ReactDOM.render(<>{element}</>, document.getElementById("root"));
+ReactDOM.render(
+    <Provider store={store}>
+        {element}
+        <Counter></Counter>
+    </Provider>,
+    document.getElementById("root"));
